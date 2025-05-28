0bb1ddc0dc551d7d6b6ee7f9b6f0e07e91307de4
kowat01 <sumin449@naver.com>
2025-05-28 22:29:09 +0900

    게시판 - 댓글 - 수정 최종완성

--- src/main/java/kr/it/academy/myapp/board/mapper/BoardFileRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.it.academy.myapp.board.mapper;
-
-import kr.it.academy.myapp.board.entity.BoardFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardFileRepository extends JpaRepository<BoardFileEntity, Integer> {
-}
